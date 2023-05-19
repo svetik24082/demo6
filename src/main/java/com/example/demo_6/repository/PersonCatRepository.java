a5acc55ae15c67bcf4f25bd5c5ad380186a8cb6d
Света <svetik24082@mail.ru>
2023-05-19 21:11:31 +0500

    3 коммит

--- src/main/java/com/example/demo_6/repository/PersonCatRepository.java
+++ src/main/java/com/example/demo_6/repository/PersonCatRepository.java
@@ -3,7 +3,6 @@ package com.example.demo_6.repository;
 import com.example.demo_6.model.PersonCat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Set;
 
 @Repository
